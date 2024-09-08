b4e5908b7148c0ccf78d632749d65034fbd83dbd
Fardin Omor Afnan <afnan.eu.cse@gmail.com>
2024-09-09 00:46:40 +0600

    service section

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,25 +1,39 @@
-import { Linkedin } from "lucide-react";
+import { Facebook, Linkedin } from "lucide-react";
 import React from "react";
+import { Button } from "./ui/button";
 
 const Footer = () => {
-  return (
-    <footer className=" section_gap bg-gradient-to-tr from-primary/10 to-primary/5 lg:rounded-3xl   ">
-      <div className="container grid grid-cols-1 gap-8 divide-y-2 divide-[#5b38cf41] ">
-        <div className="h-40"></div>
-        <div className="flex justify-between gap-4 pt-4 ">
-          <p className="tracking-wider">
-            &copy; 2024 vivasoftltd. All rights reserved.
-          </p>
+    return (
+        <footer className=" section_gap bg-gradient-to-tr from-primary/10 to-primary/5 lg:rounded-3xl   ">
+            <div className="container grid grid-cols-1 gap-8 divide-y-2 divide-[#5b38cf41] ">
+                <div className="h-40"></div>
+                <div className="flex flex-col lg:flex-row justify-between gap-4 pt-4 ">
+                    <p className="tracking-wider">
+                        &copy; {new Date().getFullYear()} vivasoftltd. All
+                        rights reserved.
+                    </p>
 
-          <div>
-            <a href="#">
-              <Linkedin className="bg-primary size-9 rounded-md p-1.5 text-white " />
-            </a>
-          </div>
-        </div>
-      </div>
-    </footer>
-  );
+                    <div className="flex items-center gap-3 ">
+                        <a className=" " href="#">
+                            <Button size={"icon"} className="">
+                                {" "}
+                                <Linkedin className=" fill-white stroke-white stroke-1 " />
+                            </Button>
+                        </a>
+                        <a
+                            className=" bg-white size-10 rounded-full flex justify-center items-center "
+                            href="#"
+                        >
+                            <Button size={"icon"} className="">
+                                {" "}
+                                <Facebook className=" fill-white stroke-white stroke-1 " />
+                            </Button>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
 };
 
 export default Footer;
---
+++ components/Home/TechStack.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const TechStack = () => {
+    return <section className="section_gap">TechStack</section>;
+};
+
+export default TechStack;
